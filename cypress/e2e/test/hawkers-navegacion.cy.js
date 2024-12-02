5f8da023aaaf74ec8c0da11188823491767ccd30
Paula Carmona <p.a.carmona.g@gmail.com>
2024-12-02 12:03:55 -0500

    Optimize navigation tests in the mobile menu

--- cypress/e2e/test/hawkers-navegacion.cy.js
+++ cypress/e2e/test/hawkers-navegacion.cy.js
@@ -5,38 +5,32 @@ describe("Pruebas de navegación", () => {
     beforeEach(() => {
         cy.visit("https://www.hawkersco.com/co/");
         cy.get("#CybotCookiebotDialogBodyLevelButtonLevelOptinAllowAll").click();
-        
     });
         
     it("Debería navegar a la sección black friday", () => {
+
         cy.get(".js-button-open-menu-mobile").click();
         
         cy.get('#menu-promotions').click({ force: true });
         cy.url().should("include", "/black-friday");
-    });
-    
-    it("Debería navegar a la sección gafas de sol", () => {
+        
         cy.get(".js-button-open-menu-mobile").should('be.visible').click();
 
         cy.get('#menu-sunglasses').click({ force: true });
         cy.url().should("include", "/gafas-de-sol");
-    });
 
-    it("Debería navegar a la sección New in", () => {
         cy.get(".js-button-open-menu-mobile").click();
 
         cy.get('#menu-new-in').click({ force: true });
         cy.url().should("include", "/new-in");
-    });
-
-    it("Debería navegar a la sección Colaboraciones", () => {
+   
+        
         cy.get(".js-button-open-menu-mobile").click();
 
         cy.get('#menu-collaborations').click({ force: true });
         cy.url().should("include", "/colaboraciones");
-    });
-
-    it("Debería navegar a la sección Accesorios", () => {
+    
+        
         cy.get(".js-button-open-menu-mobile").click();
 
         cy.get('#menu-accessories').click({ force: true });
